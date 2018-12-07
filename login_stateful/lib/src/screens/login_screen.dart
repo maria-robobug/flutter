3013f7e2a659d5f498bc505f2a0d7f17223e18d4
maria-robobug <mariashow2001@gmail.com>
2018-12-07 13:39:32 +0000

    Added email field

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -11,8 +11,28 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   Widget build(context) {
     return Container(
-      margin: EdgeInsets.all(20.0),
-      child: Text('Login Screen'),
+      margin: EdgeInsets.all(40.0),
+      child: Form(
+          child: Column(
+        children: [
+          emailField(),
+          // passwordField(),
+          // submitButton(),
+        ],
+      )),
     );
   }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {}
+
+  Widget submitButton() {}
 }
