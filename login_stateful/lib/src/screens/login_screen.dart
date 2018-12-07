f3b27d3a9eb48b49667af8b3ce12fcb3946060d3
maria-robobug <mariashow2001@gmail.com>
2018-12-07 12:04:22 +0000

    initial setup

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('Login Screen'),
+    );
+  }
+}
